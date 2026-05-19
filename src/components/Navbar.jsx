04e8191af59320610525a2d999bb3510fce62d02
banabir4870 <banabir4870@gmail.com>
2026-05-20 02:42:35 +0600

    added log out functionality and dropdown profile section when user is logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
+'use client'
 import { cinzel } from '@/app/fonts';
-import { Button } from '@heroui/react';
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Button, Dropdown, Label } from '@heroui/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
+    const userData = authClient.useSession()
+    const user = userData.data?.user;
+    console.log('userdata: ', user)
+
+    const handleLogOut = async () => {
+        await authClient.signOut()
+        redirect('/login')
+    }
     return (
         <div className='shadow-sm bg-[#d9d9d8c7] border border-b-[#4e4e35]'>
             <nav className='w-10/12 mx-auto flex justify-between items-center py-2'>
@@ -18,12 +29,36 @@ const Navbar = () => {
                         <li><Link href={'/'} className='text-[#847c6f] font-bold hover:text-[#4e4e35]'>Home</Link></li>
                         <li><Link href={'/explore-cars'} className='text-[#847c6f] font-bold hover:text-[#4e4e35]'>Explore Cars</Link></li>
                         <li><Link href={'/add-car'} className='text-[#847c6f] font-bold hover:text-[#4e4e35]'>Add Car</Link></li>
-                        <li><Link href={'/my-bookings' } className='text-[#847c6f] font-bold hover:text-[#4e4e35]'>My Bookings</Link></li>
+                        <li><Link href={'/my-bookings'} className='text-[#847c6f] font-bold hover:text-[#4e4e35]'>My Bookings</Link></li>
                     </ul>
                 </div>
                 {/* profile/login */}
                 <div>
-                    <Link href={'/login'}><Button variant='outline' className={`${cinzel.className} border-[#4e4e35] text-[#4e4e35] hover:bg-[#4e4e35] hover:text-white`}>LogIn</Button></Link>
+                    {!user ? <Link href={'/login'}><Button variant='outline' className={`${cinzel.className} border-[#4e4e35] text-[#4e4e35] hover:bg-[#4e4e35] hover:text-white`}>LogIn</Button></Link> : <Dropdown>
+                        <Button aria-label="Menu" variant="outline" className={'border border-[#4e4e35]'}>
+                            <Avatar size='sm'>
+                                <Avatar.Image referrerPolicy="no-referrer" alt={user?.name} src={user?.image} />
+                                <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
+                            </Avatar>
+                            <h2>{user?.name}</h2>
+                        </Button>
+                        <Dropdown.Popover>
+                            <Dropdown.Menu onAction={(key) => console.log(`Selected: ${key}`)}>
+                                <Dropdown.Item id="new-file" textValue="New file">
+                                    <Link href={'/add-car'}>Add Car</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item id="copy-link" textValue="Copy link">
+                                    <Link href={'/my-bookings'}>My Bookings</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item id="edit-file" textValue="Edit file">
+                                    <Link href={'/my-added-cars'}>My Added Cars</Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item id="logout" textValue="logout" onPress={handleLogOut} variant="danger" className="text-danger" color="danger">
+                                    Logout
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown.Popover>
+                    </Dropdown>}
                 </div>
             </nav>
         </div>
